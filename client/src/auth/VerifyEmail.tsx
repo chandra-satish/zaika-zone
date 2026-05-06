b2fe11fbb57b2a9fd15bb9129b58831465a19b49
Satish <89683535+softdevsatish@users.noreply.github.com>
2026-05-06 23:52:34 +0530

    fix bug

--- client/src/auth/VerifyEmail.tsx
+++ client/src/auth/VerifyEmail.tsx
@@ -82,7 +82,9 @@ const VerifyEmail = () => {
               {otp.map((digit, index) => (
                 <input
                   key={index}
-                  ref={(el) => (inputRefs.current[index] = el)}
+                  ref={(el) => {
+                    inputRefs.current[index] = el;
+                  }}
                   type="text"
                   maxLength={1}
                   inputMode="numeric"
